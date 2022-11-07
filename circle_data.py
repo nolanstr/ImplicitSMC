e25ebf8e47ba6f6cef053786df222f1647ce7c04
Nolan Strauss <nolanstrauss@nolans-mbp.lan>
2022-11-07 14:48:47 -0700

    updated from work

--- circle_data.py
+++ circle_data.py
@@ -5,12 +5,12 @@ theta = np.linspace(0, 2*np.pi, 100)
 x = np.cos(theta)
 y = np.sin(theta)
 for i in range(theta.shape[0]):
-	x[i] += np.random.normal(0, 0.0005)
+	x[i] += np.random.normal(0, 0.1)
 for i in range(theta.shape[0]):
-	y[i] += np.random.normal(0, 0.0005)
+	y[i] += np.random.normal(0, 0.1)
 
 data_x = np.zeros((100, 3))
-data_x[:,0] = x
+data_x[:,0] = x 
 data_x[:,1] = y
 data_x[:,2] = np.ones(100)
 np.save('noisycircledata', data_x)
