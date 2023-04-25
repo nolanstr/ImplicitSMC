0683d43ecbc4228f4700b67ad01e31cd3c0ee38f
Nolan Strauss <nolanstrauss@Nolans-MacBook-Pro.local>
2023-04-25 13:30:32 -0600

    work

--- gurson_test/test_string.py
+++ gurson_test/test_string.py
@@ -46,7 +46,7 @@ def run_SMC(model):
 if __name__ == "__main__":
     #[Sp, Sq, VVf]
     #[sigma_h, sigma_vm, f]
-    string = "(X_1**2) + (2 * X_2 * cosh(C_0*X_0)) - 1 - (X_2**2)" 
+    string = "(((X_2)(X_2))((X_2)(X_2)))(((X_2)(X_2))((X_2)(X_2)))" 
     x, y, z = symbols('X_0 X_1 X_2')
     shape = AGraph(equation=string)
     str(shape)
--- gurson_test/tmp.py
+++ gurson_test/tmp.py
@@ -1,47 +1,84 @@
-import numpy as np
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import sympy as sym
-from sympy import cosh
+import numpy as np
+import sympy 
+from sympy import plot_implicit, symbols, Eq, And, sympify, simplify, nsimplify
+from tqdm import tqdm
 
-from skimage.measure import marching_cubes
+from bingo.symbolic_regression.agraph.agraph import AGraph
 
-# Define the equation
-x, y, z = sym.symbols('x y z')
-eq = (y**2) + (2 * z * cosh(1.5*x)) - 1 - (z**2)
+noisy_data = np.load("noisy_gurson_data.npy")
 
-# Create a 3D meshgrid
-data = np.load("gurson_data.npy")
-idxs = np.linspace(0, data.shape[0]-1, 10, dtype=int)
-x_vals = data[idxs,0] 
-y_vals = data[idxs,1] 
-z_vals = data[idxs,2] 
-import pdb;pdb.set_trace()
-X, Y, Z = np.meshgrid(x_vals, y_vals, z_vals)
+def plot_implicit(fn, _X, _Y, _Z):
+    ''' create a plot of an implicit function
+    fn  ...implicit function (plot where fn==0)
+    bbox ..the x,y,and z limits of plotted interval'''
+    xmin, xmax = _X.min(), _X.max()
+    ymin, ymax = _Y.min(), _Y.max()
+    zmin, zmax = _Z.min(), _Z.max()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    A = np.linspace(xmin, xmax, 20) # resolution of the contour
+    B = np.linspace(ymin, ymax, 20) # number of slices
+    C = np.linspace(zmin, zmax, 20) # number of slices
+    
+    grid_X, grid_Y = np.meshgrid(_X, _Y)
+    grid_Z = np.empty(grid_X.shape)
+    shape = grid_X.shape
 
+    grid_X, grid_Y, grid_Z = grid_X.flatten(), grid_Y.flatten(), grid_Z.flatten()
+    string = "(-0.095137 + X_2)((X_2)^(-2) + X_2)" 
+    model = AGraph(equation=string)
+    x,y,z = sympy.symbols("X_0 X_1 X_2")
+    string = model.get_formatted_string(format_="sympy")
+    exp = Eq(simplify(string))
 
-# Evaluate the equation at each point in the meshgrid
-F = sym.lambdify((x, y, z), eq, 'numpy')
-S = F(X, Y, Z)
+    for i, (xval, yval) in tqdm(enumerate(zip(grid_X, grid_Y)),total=grid_X.shape[0]):
+        zval = sympy.solve(exp.subs({x:xval, y:yval}), z)
+        zval = zval[np.argmin(abs(np.array(zval)))]
+        if zval > zmax:
+            zval=np.nan
+            pass
+        grid_Z[i] = zval
+    ax.set_zlim3d(zmin,zmax)
+    ax.set_xlim3d(xmin,xmax)
+    ax.set_ylim3d(ymin,ymax)
+    grid_X = grid_X.reshape(shape)
+    grid_Y = grid_Y.reshape(shape)
+    grid_Z = grid_Z.reshape(shape)
 
-# Create the plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-#ax.set_xlim([x_vals.min(), x_vals.max()])
-#ax.set_ylim([y_vals.min(), y_vals.max()])
-#ax.set_zlim([z_vals.min(), z_vals.max()])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('Implicit equation plot')
+    ax.plot_surface(grid_X, grid_Y, grid_Z, 
+                edgecolor='royalblue', lw=0.5,
+                alpha=0.3) 
+    ax.contourf(grid_X, grid_Y, grid_Z, 
+            zdir='z', offset=zmin, cmap='coolwarm')
+    #ax.contourf(grid_X, grid_Y, grid_Z, 
+    #        zdir='x', offset=xmin, cmap='coolwarm')
+    #ax.contourf(grid_X, grid_Y, grid_Z, 
+    #        zdir='y', offset=ymax, cmap='coolwarm')
+    #ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax), zlim=(zmin, zmax),
+    #   xlabel='X', ylabel='Y', zlabel='Z')
+    x,y,z = noisy_data[:,0], noisy_data[:,1], noisy_data[:,2]
+    ax.scatter(x, y, z, c='k')
+    plt.legend()
+    plt.show()
 
-# Extract the surface where eq = 0
-verts, faces, _, _ = marching_cubes(S, 0)
+def model(x,y,z):
+    #[Sp, Sq, VVf]
+    #[sigma_h, sigma_vm, f]
+    X_0, X_1, X_2 = x, y, z
+    return (X_2**2) ** (np.cosh(np.cosh(X_1)))
+if __name__ == "__main__":
+    N = 20
+    maxs = noisy_data.max(axis=0)
+    mins = noisy_data.min(axis=0)
+    
+    p = lambda x: 10*pow((x-0.5),2) + 0
+    d = np.append(0, p(np.linspace(0,1,2*N)))
+    d /= d.sum()
+    d = np.cumsum(d)
 
-# Scale and shift the vertices to match the meshgrid
-verts *= (x_vals[1] - x_vals[0])
-verts += np.array([x_vals[0], y_vals[0], z_vals[0]])
-
-# Plot the surface
-ax.plot_trisurf(verts[:, 0], verts[:, 1], faces, verts[:, 2], cmap='viridis')
-plt.show()
+    X = mins[0] + d*abs(maxs[0]-mins[0]) 
+    Y = np.linspace(mins[1], maxs[1],N) 
+    Z = np.linspace(mins[2], maxs[2],N) 
+    plot_implicit(model, X, Y, Z)
