51eac09e634ef30eb70efa486290a60a56f26b4c
nstraus7 <nstraus7@login01.cm.cluster>
2023-11-16 18:48:40 -0500

    more test code

---
+++ ImplicitFitness/elliptical_runs/test_code/torch_hessian.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pickle
+import glob
+import time
+
+from bingo.symbolic_regression import ExplicitRegression, \
+                                      ExplicitTrainingData
+from bingo.local_optimizers.local_opt_fitness import LocalOptFitnessFunction
+from bingo.local_optimizers.scipy_optimizer import ScipyOptimizer
+from bingo.symbolic_regression.implicit_regression import ImplicitRegression, \
+                                            ImplicitTrainingData, MLERegression
+from bingo.symbolic_regression.bayes_fitness.mvn_implicit_bayes_fitness_function \
+                    import MVNImplicitBayesFitnessFunction as IBFF
+from bingo.symbolic_regression.bayes_fitness.implicit_bff_laplace \
+                    import ImplicitLaplaceBayesFitnessFunction as ILBFF
+from bingo.symbolic_regression.agraph.pytorch_agraph import PytorchAGraph
+                    
+
+PARTICLES = 20
+MCMC_STEPS = 5
+ESS_THRESHOLD = 0.7
+
+def test_true_model():
+    noise_levels = ["005"]
+
+    for i, noise_level in enumerate(noise_levels):
+
+
+        data = np.load(
+    f"../../../data/elliptic_data/elliptic_data_{noise_level}.npy")
+        data = data[~np.isnan(data.sum(axis=1)),:]
+        implicit_data = ImplicitTrainingData(data, np.empty_like(data))
+        fitness = MLERegression(implicit_data, order="second")
+        optimizer = ScipyOptimizer(fitness, method='L-BFGS-B', 
+                        param_init_bounds=[-1.,1.], options={'maxiter':500})
+        MLEclo = LocalOptFitnessFunction(fitness, optimizer)
+        
+        ibff = IBFF(PARTICLES, MCMC_STEPS, ESS_THRESHOLD, 
+                implicit_data, MLEclo, ensemble=1)
+        ilbff = ILBFF(PARTICLES, MCMC_STEPS, ESS_THRESHOLD, 
+                implicit_data, MLEclo)
+        string = "X_0^3 + X_0 - X_1^2 - 1.0"
+        print(f"Noise Level: {noise_level}")
+        model = PytorchAGraph(equation=string)
+        print(f"SSQE from MLE Regression: {MLEclo(model)}")
+
+        print(f"Model w/ MLE Parameters: {str(model)}")
+        model = PytorchAGraph(equation=string)
+        stime = time.time()
+        #print(f"\n-NMLL from iSMC: {ibff(model)}")
+        print(f"iSMC Computation Time = {time.time()-stime}")
+         
+        model = PytorchAGraph(equation=string)
+        stime = time.time()
+        print(f"\n-NMLL from iSMC(Laplace Approximation): {ilbff(model)}")
+        print(f"iSMC(Laplace Approximation) Computation Time = {time.time()-stime}\n")
+
+
+if __name__ == '__main__':
+    test_true_model()
+
